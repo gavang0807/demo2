342e797fc361e05b889b2de0847c5dc23f3ff299
gavang0807 <vincent.duong@vmodev.com>
2022-05-22 11:57:45 +0700

    fix front-end

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,54 +1,51 @@
 package com.example.demo.controller;
 
-
-
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @Controller
 public class ProductController {
-
     @Autowired
     ProductService productService;
 
-    @GetMapping("/admin/products")
+    @GetMapping("/products")
     public String getPro(Model model){
         model.addAttribute("products", productService.getAllProduct());
         return "products";
-    }//view all products
-
-    @GetMapping("/admin/products/add")
+    }
+    @GetMapping("/products/add")
     public String getProAdd(Model model){
         model.addAttribute("productDTO", new Product());
         return "productsAdd";
-    }// form add new product
+    }
+    @PostMapping("/products/add")
+    public String postProAdd(@ModelAttribute("productDTO") Product productDTO)  {
 
-    @GetMapping("/admin/products/delete/{id}")
+        Product product = new Product();
+        product.setId(productDTO.getId());
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setQuantity(productDTO.getQuantity());
+        product.setDescription(productDTO.getDescription());
+        productService.updateProduct(product);
+        return "redirect:/admin/products";
+    }
+    @GetMapping("/products/delete/{id}")
     public String deletePro(@PathVariable long id){
         productService.removeProductById(id);
         return "redirect:/admin/products";
-    }//delete 1 product
+    }
 
-    @GetMapping("/admin/products/update/{id}")
+    @GetMapping("/products/update/{id}")
     public String updatePro(@PathVariable long id, Model model){
         Optional<Product> opProduct = productService.getProductById(id);
         if (opProduct.isPresent()){
             Product product = opProduct.get();
-
             Product productDTO = new Product();
             productDTO.setId(product.getId());
             productDTO.setName(product.getName());
@@ -61,7 +58,5 @@ public class ProductController {
         }else {
             return "404";
         }
-
-    }//form edit product, fill old data into form
-
+    }
 }
